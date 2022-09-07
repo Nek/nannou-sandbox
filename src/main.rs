bafcb8ddc4a40a9367de8bee984a2f803c26b13b
Nikita Dudnik <nikdudnik@gmail.com>
2022-09-08 00:44:19 +0200

    Make audio work with glitches

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,166 @@
+use std::{
+    sync::mpsc::{channel, Receiver, Sender},
+    thread,
+    time::Duration,
+};
+
 use libm::floorf;
 use nannou::event::ElementState;
 use nannou::prelude::*;
 
 use pad::PadStr;
 
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
+fn start_audio() -> Sender<f32> {
+    let (tx, rx): (Sender<f32>, Receiver<f32>) = channel();
+    thread::spawn(|| -> anyhow::Result<()> {
+        let stream = stream_setup_for(sample_next, rx)?;
+        stream.play()?;
+        thread::sleep(Duration::MAX);
+        Ok(())
+    });
+    tx
+}
+
+fn sample_next(o: &mut SampleRequestOptions) -> f32 {
+    o.tick();
+    let mut res = o.last_freq();
+    loop {
+        match o.rx.try_recv() {
+            Ok(v) => res = v,
+            Err(_) => break,
+        };
+    }
+    println!("{}", res);
+    o.set_last_freq(res);
+    o.tone(res) * 0.1 + o.tone(880.) * 0.1
+    // combination of several tones
+}
+
+pub struct SampleRequestOptions {
+    pub sample_rate: f32,
+    pub sample_clock: f32,
+    pub nchannels: usize,
+    pub rx: Receiver<f32>,
+    pub last_freq: f32,
+}
+
+impl SampleRequestOptions {
+    fn tone(&self, freq: f32) -> f32 {
+        (self.sample_clock * freq * 2.0 * std::f32::consts::PI / self.sample_rate).sin()
+    }
+    fn tick(&mut self) {
+        self.sample_clock = (self.sample_clock + 1.0) % self.sample_rate;
+    }
+
+    pub fn set_last_freq(&mut self, last_freq: f32) {
+        self.last_freq = last_freq;
+    }
+
+    pub fn last_freq(&self) -> f32 {
+        self.last_freq
+    }
+}
+
+pub fn stream_setup_for<F>(on_sample: F, rx: Receiver<f32>) -> Result<cpal::Stream, anyhow::Error>
+where
+    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static + Copy,
+{
+    let (_host, device, config) = host_device_setup()?;
+
+    let sample_rate = config.sample_rate().0 as f32;
+    let sample_clock = 0f32;
+    let nchannels = config.channels() as usize;
+    let request = SampleRequestOptions {
+        rx,
+        sample_rate,
+        sample_clock,
+        nchannels,
+        last_freq: 440.,
+    };
+
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => {
+            stream_make::<f32, _>(&device, &config.into(), on_sample, request)
+        }
+        cpal::SampleFormat::I16 => {
+            stream_make::<i16, _>(&device, &config.into(), on_sample, request)
+        }
+        cpal::SampleFormat::U16 => {
+            stream_make::<u16, _>(&device, &config.into(), on_sample, request)
+        }
+    }
+}
+
+pub fn host_device_setup(
+) -> Result<(cpal::Host, cpal::Device, cpal::SupportedStreamConfig), anyhow::Error> {
+    let host = cpal::default_host();
+
+    let device = host
+        .default_output_device()
+        .ok_or_else(|| anyhow::Error::msg("Default output device is not available"))?;
+    println!("Output device : {}", device.name()?);
+
+    let config = device.default_output_config()?;
+    println!("Default output config : {:?}", config);
+
+    Ok((host, device, config))
+}
+
+pub fn stream_make<T, F>(
+    device: &cpal::Device,
+    config: &cpal::StreamConfig,
+    on_sample: F,
+    mut request: SampleRequestOptions,
+) -> Result<cpal::Stream, anyhow::Error>
+where
+    T: cpal::Sample,
+    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static + Copy,
+{
+    let err_fn = |err| eprintln!("Error building output sound stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |output: &mut [T], _: &cpal::OutputCallbackInfo| {
+            on_window(output, &mut request, on_sample)
+        },
+        err_fn,
+    )?;
+
+    Ok(stream)
+}
+
+fn on_window<T, F>(output: &mut [T], request: &mut SampleRequestOptions, mut on_sample: F)
+where
+    T: cpal::Sample,
+    F: FnMut(&mut SampleRequestOptions) -> f32 + std::marker::Send + 'static,
+{
+    for frame in output.chunks_mut(request.nchannels) {
+        let value: T = cpal::Sample::from::<f32>(&on_sample(request));
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
+}
 
 fn main() {
     nannou::app(model).event(event).update(update).run();
 }
 
 struct Model {
+    tx: Sender<f32>,
     _window: window::Id,
     allowed_segments: [(i32, i32); 16],
     id: u16,
 }
 
 fn model(app: &App) -> Model {
+    let tx = start_audio();
+
     let _window = app.new_window().size(600, 600).view(view).build().unwrap();
     Model {
+        tx,
         _window,
         allowed_segments: [
             (0, 1),
@@ -57,7 +200,7 @@ fn get_segment_xys((a, b): (Vec2, Vec2), rect: Rect) -> (Vec2, Vec2) {
     (get_dot_xy(a, rect), get_dot_xy(b, rect))
 }
 
-fn event(_app: &App, model: &mut Model, event: Event) {
+fn event(app: &App, model: &mut Model, event: Event) {
     match event {
         Event::DeviceEvent(_id, event) => match event {
             nannou::winit::event::DeviceEvent::Button { button, state } => {
@@ -66,13 +209,7 @@ fn event(_app: &App, model: &mut Model, event: Event) {
                 }
             }
             nannou::winit::event::DeviceEvent::MouseMotion { delta: _ } => {
-                /*
-                let res = sender.send((app.mouse.y + 300.) / 600.);
-                match res {
-                    Ok(_) => (),
-                    Err(_) => (),
-                }
-                */
+                model.tx.send((app.mouse.y + 300.) / 600. * 1000. + 1000.).unwrap();
                 ()
             }
             _ => (),
